7717e4748cc485f75c1457457da178f14f9fc39c
adwISAry <mi75axs6d@code.edu.az>
2023-12-07 13:33:53 +0400

    ureyim istiyir

--- Areas/Admin/Controllers/ProductController.cs
+++ Areas/Admin/Controllers/ProductController.cs
@@ -3,6 +3,8 @@ using Microsoft.EntityFrameworkCore;
 using nov30task.Areas.Admin.ViewModels;
 using nov30task.Context;
 using nov30task.Models;
+using nov30task.ViewModels.BlogVM;
+using nov30task.ViewModels.CategoryVM;
 using nov30task.ViewModels.ProductVM;
 
 namespace nov30task.Areas.Admin.Controllers
@@ -20,7 +22,7 @@ namespace nov30task.Areas.Admin.Controllers
 
 		public IActionResult Index()
 		{
-			return View(_db.Products.Select(p => new AdminProductListItemVM{
+			return View(_db.Products.Select(p => new AdminProductListItemVM {
 				Id = p.Id,
 				Name = p.Name,
 				CostPrice = p.CostPrice,
@@ -41,26 +43,26 @@ namespace nov30task.Areas.Admin.Controllers
 
 		[HttpPost]
 
-        public async Task<IActionResult> Create(ProductCreateVM vm)
-        {
+		public async Task<IActionResult> Create(ProductCreateVM vm)
+		{
 
 			if (vm.CostPrice > vm.SellPrice)
-            {
+			{
 				ModelState.AddModelError("SellPrice", "SellPrice > CostPrice olmalidir.");
-            }
-            if (!ModelState.IsValid)
-            {
-                ViewBag.Categories = _db.Categories;
-                return View(vm);
-            }
-            if (!await _db.Categories.AnyAsync(c => c.Id == vm.CategoryId))
-            {
-                ModelState.AddModelError("CategoryId", "Bele kateqoriya yoxdur.");
-                ViewBag.Categories = _db.Categories;
-                return View(vm);
-            }
+			}
+			if (!ModelState.IsValid)
+			{
+				ViewBag.Categories = _db.Categories;
+				return View(vm);
+			}
+			if (!await _db.Categories.AnyAsync(c => c.Id == vm.CategoryId))
+			{
+				ModelState.AddModelError("CategoryId", "Bele kateqoriya yoxdur.");
+				ViewBag.Categories = _db.Categories;
+				return View(vm);
+			}
 
-            Product product = new Product
+			Product product = new Product
 			{
 				Name = vm.Name,
 				About = vm.About,
@@ -71,14 +73,84 @@ namespace nov30task.Areas.Admin.Controllers
 				Quantity = vm.Quantity,
 				Description = vm.Description,
 				Discount = vm.Discount,
-				ImageUrl = vm.ImageUrl,
 				CategoryId = vm.CategoryId
 			};
 
 			await _db.Products.AddAsync(product);
 			await _db.SaveChangesAsync();
 
+			return RedirectToAction(nameof(Index));
+		}
+		public async Task<IActionResult> Update(int id)
+		{
+			if (id == null || id <= 0) return BadRequest();
+			var data = await _db.Products.FindAsync(id);
+			ViewBag.Categories = _db.Categories;
+			if (data == null) return NotFound();
+			return View(new ProductUpdateVM
+			{
+				Name = data.Name,
+				About = data.About,
+				Avability = data.Avability,
+				Brand = data.Brand,
+				CategoryId = data.CategoryId,
+				CostPrice = data.CostPrice,
+				Description = data.Description,
+				Discount = data.Discount,
+				ExTax = data.ExTax,
+				ProductCode = data.ProductCode,
+				Quantity = data.Quantity,
+				RewardPoints = data.RewardPoints,
+				SellPrice = data.SellPrice,
+			});
+
+		}
+		[HttpPost]
+
+		public async Task<IActionResult> Update(int? id, ProductUpdateVM vm)
+		{
+			TempData["ProductRenovationResponse"] = false;
+
+			if (id == null || id <= 0) return BadRequest();
+			if (!ModelState.IsValid)
+			{
+				return View(vm);
+			}
+			var data = await _db.Products.FindAsync(id);
+			if (data == null) return NotFound();
+
+			data.Name = vm.Name;
+			data.About = vm.About;
+			data.Avability = vm.Avability;
+			data.Brand = vm.Brand;
+			data.CostPrice = vm.CostPrice;
+			data.Description = vm.Description;
+			data.Discount = vm.Discount;
+			data.ExTax = vm.ExTax;
+			data.ProductCode = (string?)vm.ProductCode;
+			data.Quantity = vm.Quantity;
+			data.RewardPoints = vm.RewardPoints;
+			data.SellPrice = vm.SellPrice;
+			data.CategoryId = vm.CategoryId;
+
+			TempData["BookRenovationResponse"] = true;
+			await _db.SaveChangesAsync();
+			return RedirectToAction(nameof(Index));
+		}
+
+		public async Task<IActionResult> Delete(int? id)
+        {
+            TempData["BookDeletionResponse"] = false;
+            if (id == null) return BadRequest();
+            var data = await _db.Products.FindAsync(id);
+            if (data == null) return NotFound();
+            _db.Products.Remove(data);
+            await _db.SaveChangesAsync();
+            TempData["BookDeletionResponse"] = true;
             return RedirectToAction(nameof(Index));
         }
+        
+
     }
+
 }
---
+++ ViewModels/ProductVM/ProductUpdateVM.cs
@@ -0,0 +1,19 @@
+﻿namespace nov30task.ViewModels.ProductVM
+{
+    public class ProductUpdateVM
+    {
+        public string Name { get; set; }
+        public string About { get; set; }
+        public string Avability { get; set; }
+        public string Brand { get; set; }
+        public int CategoryId { get; set; }
+        public decimal CostPrice { get; set; }
+        public string Description { get; set; }
+        public float Discount { get; set; }
+        public decimal ExTax { get; set; }
+        public object ProductCode { get; set; }
+        public ushort Quantity { get; set; }
+        public string RewardPoints { get; set; }
+        public decimal SellPrice { get; set; }
+    }
+}
