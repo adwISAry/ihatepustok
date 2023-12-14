e65d45ad30cc40b2cabb8e7f63b3b947ac46ff30
adwISAry <mi75axs6d@code.edu.az>
2023-12-14 13:40:09 +0400

    View Component

---
+++ ViewComponents/SliderViewComponent.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using nov30task.Context;
+using nov30task.ViewModels.SliderVM;
+
+namespace nov30task.ViewComponents
+{
+    public class SliderViewComponent : ViewComponent
+    {
+        public SliderViewComponent(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        ApplicationDbContext _context { get; }
+
+
+        public async Task <IViewComponentResult> InvokeAsync()
+        {
+            ;
+
+            return View(await _context.Sliders.Select(s=> new SliderListItemVM
+            {
+                Id = s.Id,
+                ImageUrl = s.ImageUrl,
+                IsLeft = s.IsLeft,
+                Title = s.Title,
+                Text = s.Text,
+            }).ToListAsync());
+        }
+    }
+}
