69e2ea6ce9863121a33ad8d0dcc673cf180af9df
adwISAry <mi75axs6d@code.edu.az>
2023-12-20 04:23:36 +0400

    reg

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,12 +1,56 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using nov30task.Models;
+using nov30task.ViewModels.AuthVM;
+using Microsoft.AspNetCore.Identity;
 
 namespace nov30task.Controllers
 {
     public class AuthController : Controller
     {
+
+
+        SignInManager<AppUser> _signInManager { get; }
+        UserManager<AppUser> _userManager { get; }
+        public AuthController(SignInManager<AppUser> signInManager, UserManager<AppUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+
+
         public IActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> Register(RegisterVM vm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(vm);
+            }
+
+            var result = await _userManager.CreateAsync(new AppUser
+            {
+                UserName = vm.UserName,
+                Email = vm.Email,
+            }, vm.Password);
+
+            if (!result.Succeeded)
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+
+                return View(vm);
+            }
+
+            return RedirectToAction("RegistrationSuccess");
         }
     }
 }
--- ViewModels/AuthVM/RegisterVM.cs
+++ ViewModels/AuthVM/RegisterVM.cs
@@ -1,12 +1,17 @@
-﻿namespace nov30task.ViewModels.AuthVM
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace nov30task.ViewModels.AuthVM
 {
     public class RegisterVM
     {
+        [Required(ErrorMessage = "Ad ve soyadinizi duzgun qeyd edin!"),MinLength(4), MaxLength(24)]
         public string UserName { get; set; }
+        [Required, DataType(DataType.EmailAddress)]
         public string Email { get; set; }
-
+    
+        [Required, DataType(DataType.Password), Compare(nameof(ConfirmPassword)), RegularExpression("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]). {4,}$")]
         public string Password { get; set; }
-
+        [Required, DataType(DataType.Password), RegularExpression("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]). {4,}$")]
         public string ConfirmPassword { get; set; }
     }
 }
